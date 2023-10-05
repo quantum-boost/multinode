dc7f8a5d788e544b9aa821528480362976e115db
Kacper Kielak <kielak.kacper@gmail.com>
2023-10-05 17:06:24 +0100

    Add basic login functionality

---
+++ python-client/multinode/config.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel
+
+CONFIG_FILE_PATH = Path().home() / ".multinode" / "config.json"
+
+DEFAULT_API_URL = "https://control.quantumboost-development.com"
+
+
+class Config(BaseModel):
+    api_url: str = DEFAULT_API_URL
+    api_key: Optional[str] = None
+
+
+def load_config() -> Config:
+    if CONFIG_FILE_PATH.exists():
+        with CONFIG_FILE_PATH.open("r") as f:
+            return Config.parse_raw(f.read())
+
+    return Config.parse_raw("{}")
+
+
+def save_config(config: Config) -> None:
+    CONFIG_FILE_PATH.parent.mkdir(exist_ok=True)
+
+    with CONFIG_FILE_PATH.open("w") as f:
+        f.write(config.json(exclude_none=True, exclude_unset=True))
--- python-client/multinode/main.py
+++ python-client/multinode/main.py
@@ -1,14 +1,73 @@
+from getpass import getpass
+
 import click
+
+from multinode.api_client import ApiClient, Configuration, DefaultApi
+from multinode.api_client.exceptions import ForbiddenException
+from multinode.config import load_config, save_config
+
+API_KEY_MISSING_MSG = click.style(
+    "You are not logged in. Run `multinode login` first.", fg="red"
+)
+
+INVALID_API_KEY_MSG = click.style("API key is invalid.", fg="red")
+SUCCESSFUL_LOGIN_MSG = click.style("You have successfully logged in!", fg="green")
+
+SUCCESSFUL_LOGOUT_MSG = "You have successfully logged out."
 
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx: click.Context):
+    # If no command is provided, or user tries to log in or log out,
+    # proceed with click's default behavior
+    if ctx.invoked_subcommand is None or ctx.invoked_subcommand in ["login", "logout"]:
+        return
+
+    # Otherwise, check if user is logged in
+    config = load_config()
+    if config.api_key is None:
+        click.echo(API_KEY_MISSING_MSG)
+        ctx.exit(code=1)
 
 
 @cli.command()
-def login():
-    raise NotImplementedError
+@click.pass_context
+def login(ctx: click.Context):
+    multinode_config = load_config()
+    if multinode_config.api_key is not None:
+        click.echo(
+            "You are already logged in. If you want to log in with "
+            "a different account, run `multinode logout` first."
+        )
+        return
+
+    # Current login flow just asks for API key directly. Alternatives include
+    # redirecting to an authenticated web session or exchanging username/password for
+    # an API key. Both require additional work and are not necessary for the MVP.
+    api_key = getpass("Enter your API key:")
+    multinode_config.api_key = api_key
+
+    # Check if the API key is valid
+    client_config = Configuration(host=multinode_config.api_url, access_token=api_key)
+    client = ApiClient(client_config)
+    api = DefaultApi(client)
+    try:
+        api.list_projects_projects_get()
+    except ForbiddenException:
+        click.echo(INVALID_API_KEY_MSG)
+        ctx.exit(code=1)
+
+    save_config(multinode_config)
+    click.echo(SUCCESSFUL_LOGIN_MSG)
+
+
+@cli.command()
+def logout():
+    config = load_config()
+    config.api_key = None
+    save_config(config)
+    click.echo(SUCCESSFUL_LOGOUT_MSG)
 
 
 @cli.command()
