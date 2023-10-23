35b26ae2cc2cdbc21548d6dfaa2950c8cb6a7b89
Kacper Kielak <kielak.kacper@gmail.com>
2023-10-23 17:57:10 +0100

    Allow user to specify API url when logging in
    Closes #77

--- python-client/multinode/constants.py
+++ python-client/multinode/constants.py
@@ -3,6 +3,5 @@ from pathlib import Path
 LATEST_VERSION_STR = "latest"
 
 CONFIG_FILE_PATH = Path().home() / ".multinode" / "config.json"
-DEFAULT_API_URL = "https://control.quantumboost-development.com"
 
 ROOT_WORKER_DIR = "/app"
